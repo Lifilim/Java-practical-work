fc875d4e4f2685aca7e94d784922abc9a10fdd4a
Anastasia Gozheva <d1804a@gmail.com>
2025-05-03 18:42:03 +0400

    try database

--- RandomWeather/src/main/java/org/example/Main.java
+++ RandomWeather/src/main/java/org/example/Main.java
@@ -6,10 +6,42 @@ import org.example.exceptions.WrongCityNameException;
 import org.example.services.WeatherService;
 import org.example.services.impl.WeatherServiceImpl;
 
+//import org.example.repositories.DatabaseExample;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+            String url="jdbc:postgresql://localhost:5432/weather";
+            String user="lim";
+            String password="milmil";
+
+            try(Connection connection = DriverManager.getConnection(url, user, password)) {
+                Statement statement = connection.createStatement();
+                InputStream sqlInitial = Main.class
+                        .getClassLoader()
+                        .getResourceAsStream("sql/schema.sql");
+                assert sqlInitial != null;
+                String sql = new String(sqlInitial.readAllBytes(), StandardCharsets.UTF_8);
+                statement.execute(sql);
+                System.out.println("подключились!");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
         try (Scanner terminalInput = new Scanner(System.in)) {
             WeatherService weatherService = new WeatherServiceImpl();
             while (true) {
